0d0cde27875f2f6f59020091194710a2eede1aeb
Jecced <jecced@gmail.com>
2020-04-03 13:48:15 +0800

    Improve work

--- assets/ShaderResolutionHelper.ts
+++ assets/ShaderResolutionHelper.ts
@@ -6,19 +6,14 @@ export default class ShaderTimerHelper extends cc.Component {
 
     private material:cc.Material = null;
 
-    private time:number = 0;
-
     protected onLoad () {
         let sprite = this.node.getComponent(cc.Sprite);
         if (!sprite) {
             return;
         }
         this.material = sprite.getMaterial(0);
-    }
-
-    protected update(dt:number):void{
-        if(!this.material) return;
-        this.time += dt;
-        this.material.effect.setProperty('u_time', this.time);
+        // 用来传递 demo 中 u_resolution 值 这个值被初始化之后就不会变化,所以只用在onLoad阶段传递一次即可
+        let csize = this.node.getContentSize();
+        this.material.effect.setProperty('u_resolution', [csize.width, csize.height]);
     }
 }
