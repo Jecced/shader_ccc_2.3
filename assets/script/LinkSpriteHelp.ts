77989eeea3ea7fec0fb1ce87a49625e5df83e365
Jecced <jecced@gmail.com>
2020-04-12 11:45:34 +0800

    update link-prefab tools

---
+++ assets/script/LinkSpriteHelp.ts
@@ -0,0 +1,43 @@
+import LinkPrefab from './LinkPrefab';
+/*********************************
+ * 创建者   ：Relvin
+ * 时间     ：2020年3月11号16:52:10
+ * 
+ * ******************************/
+
+
+const {ccclass, executeInEditMode, property} = cc._decorator;
+
+@ccclass
+@executeInEditMode
+export default class LinkSpriteHelp extends cc.Component {
+
+    @property
+    private _spriteFrame: cc.SpriteFrame = null;
+
+    @property({type: cc.SpriteFrame, visible: true, displayName: "纹理图"})
+    set spriteFrame(value: cc.SpriteFrame) {
+        this._spriteFrame = value
+        this._updateSpriteFrame()
+    }
+
+    get spriteFrame(): cc.SpriteFrame {
+        return this._spriteFrame
+    }
+
+    start () {
+
+    }
+
+    onLoad() {
+        this._updateSpriteFrame()
+    }
+
+    private _updateSpriteFrame() {
+        let linkPrefab = this.node.getComponent(LinkPrefab)
+        let sprite = linkPrefab.getPrefabComponect(cc.Sprite)
+        if (sprite) {
+            sprite.spriteFrame = this._spriteFrame
+        }
+    }
+}
